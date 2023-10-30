73274ceec6f9cf65a2fe6be2d784ca7d74995e3e
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-30 11:22:53 -0500

    initial commit

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className=" text-blue-400 text-2xl text-center px-8 font-bold">Time to build a React Native App with Tailwind!</Text>
+    </View>
+  );
+}
