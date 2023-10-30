6eee5ef549b3715611efd03303a5c1de4b85d682
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-30 17:24:16 -0500

    moving nav wrapper

--- components/TabBar.jsx
+++ components/TabBar.jsx
@@ -3,16 +3,26 @@ import { useEffect, useState } from 'react'
 
 
 export default function TabBar({ state, descriptors, navigation }) {
-  const { height, width } = useWindowDimensions();
+  const { width } = useWindowDimensions();
   const [ tabWidth, setTabWidth ] = useState(width - 32);
 
   function activeBackgroundColor(tab) {
-    if (tab === 'Home') {
+    if (tab === 0) {
       return '#91A5CB'
-    } else if (tab === 'Sets') {
+    } else if (tab === 1) {
       return '#E9764C'
-    } else if (tab === 'Songs') {
+    } else if (tab === 2) {
       return '#FFB56E'
+    }
+  }
+
+  function highlightPosition(tab) {
+    if (tab === 0) {
+      return 'flex-start'
+    } else if (tab === 1) {
+      return 'center'
+    } else if (tab === 2) {
+      return 'flex-end'
     }
   }
 
@@ -34,59 +44,83 @@ export default function TabBar({ state, descriptors, navigation }) {
       width: 84, 
       borderRadius: 8, 
       justifyContent: 'center', 
-      alignItems: 'center' }
+      alignItems: 'center',
+      zIndex: 1,
+    },
+    navHighlight: {
+      backgroundColor: activeBackgroundColor(state.index),
+      zIndex: 0,
+      height: 32,
+      width: 84, 
+      borderRadius: 8, 
+    },
+    navHighlightWrapper: {
+      bottom: 12,
+      left: 12,
+      position: 'absolute',
+      width: width-58,
+      flexDirection: 'row',
+      transform:
+    }
   })
 
   return (
-    <View style={styles.navBar}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+    <>
 
-        const isFocused = state.index === index;
+      <View style={styles.navBar}>
+        <View style={[styles.navHighlightWrapper, { justifyContent: highlightPosition(state.index) }]}>
+          <View style={styles.navHighlight}/>
+        </View>
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
-          }
-        };
+          const isFocused = state.index === index;
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
 
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={[styles.buttonWrapper, { backgroundColor: isFocused ? activeBackgroundColor(label) : '#2F3253' }]}
-            key={label}
-          >
-            <Text style={{fontFamily: 'Poppins_700Bold', color: isFocused ? '#2F3253' : '#fff'}}>
-              {label}
-            </Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <TouchableOpacity
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={[styles.buttonWrapper]}
+              key={label}
+            >
+              <Text style={{ zIndex: 2, fontFamily: 'Poppins_700Bold', color: isFocused ? '#2F3253' : '#fff'}}>
+                {label}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </>
   )
 }
 
