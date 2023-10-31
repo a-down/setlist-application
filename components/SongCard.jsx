93a0adfeec286ee7978bff28e8cb21da61b8d886
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-31 11:28:00 -0500

    slim songCard

--- components/SongCard.jsx
+++ components/SongCard.jsx
@@ -9,8 +9,8 @@ export default function SongCard({ slim = false }) {
     return (
       <View className="flex-row justify-between items-center bg-white w-full rounded-[10px] shadow-brand p-4 overflow-clip border-l-[10px] border-brand-yellow">
 
-        <View style={{gap: 12}}>
-          <Text className="text-xl font-bold text-brand-navy">Come On Get Higher</Text>
+        <View>
+          <Text className="text-xl font-bold text-brand-navy mt-1">Come On Get Higher</Text>
           <Text className="text-brand-navy opacity-80">Matt Nathanson</Text>
         </View>
         
--- screens/SongsScreen.jsx
+++ screens/SongsScreen.jsx
@@ -1,23 +1,66 @@
-import { Text, View, ScrollView, ImageBackground, Image, Pressable } from 'react-native';
-
+import { Text, View, ScrollView, ImageBackground, Image, Pressable, useWindowDimensions } from 'react-native';
+import SongCard from '../components/SongCard';
 
 
 export default function SongsScreen() {
-
+  const { width } = useWindowDimensions();
 
   return (
-    <>
-      <View className="flex-1 bg-brand-navy">
-        <View className="flex-row m-4 justify-between items-end h-20">
-          <View className="bg-brand-yellow h-10 w-10 rounded-full" />
-          <View className="bg-brand-yellow h-10 w-10 rounded-full" />
-        </View>
-
-        <ScrollView className="bg-gray-background flex-1 rounded-t-[20px] p-4">
-          <Text>Songs</Text>
-          
-        </ScrollView>
+    <View className="flex-1 bg-brand-navy">
+      <View className="flex-row m-4 justify-between items-end h-20">
+        <View className="bg-brand-yellow h-10 w-10 rounded-full" />
+        <View className="bg-brand-yellow h-10 w-10 rounded-full" />
       </View>
-    </>
+
+      <ScrollView className="bg-gray-background flex-1 rounded-t-[20px] p-4">
+        <View style={{ marginBottom: 16}}>
+          <View className=" flex-row justify-between mb-4">
+            <Text className="text-brand-navy font-bold text-2xl mt-4">Sets</Text>
+
+            {/* <Pressable 
+              className="bg-brand-orange rounded-[10px] flex-row p-4 items-center active:bg-[#E99170]" style={{gap: 8}}
+              onPress={() => Alert.alert('New set form')}>
+              <View className="w-5 h-5 rounded-full bg-gray-background " />
+              <Text className="text-gray-background font-bold text-xl mt-1">Add Song</Text>
+            </Pressable> */}
+          </View>
+
+          <View className="bg-white shadow-brand rounded-full p-3 flex-row items-center mb-8" style={{width: width-32, gap: 8}}>
+            <View className="w-6 aspect-square bg-brand-navy opacity-60 rounded-full"/>
+            <Text className="mt-1 font-semibold text-brand-navy opacity-60">Search for a set</Text>
+          </View>
+          
+          <View style={{gap: 8, marginBottom: 32}}>
+            <View className="flex-row justify-between w-full">
+              <Text className="text-brand-navy text-xl mb-2">Your Songs</Text>
+              <Text className="text-brand-navy opacity-60 text-xl mb-2">Most Recent</Text>
+            </View>
+
+            <SongCard slim={true}/>
+            <SongCard slim={true}/>
+
+          </View>
+
+          <View style={{gap: 8, marginBottom: 128}}>
+            <View className="flex-row justify-between w-full">
+              <Text className="text-brand-navy text-xl mb-2">All Saved Songs</Text>
+              <Text className="text-brand-navy opacity-60 text-xl mb-2">Most Recent</Text>
+            </View>
+            
+
+            <SongCard slim={true}/>
+            <SongCard slim={true}/>
+            <SongCard slim={true}/>
+            <SongCard slim={true}/>
+            <SongCard slim={true}/>
+
+          </View>
+
+        </View>
+        
+      </ScrollView>
+
+
+    </View>
   )
 }
