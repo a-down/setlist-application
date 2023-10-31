67e4e9e8c06111afedeced62c6d1ffa92148b271
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-31 11:32:49 -0500

    slim card updates

--- components/SetCard.jsx
+++ components/SetCard.jsx
@@ -21,7 +21,7 @@ export default function SetCard({ slim = false }) {
       <View className="flex-row justify-between items-center bg-white w-full rounded-[10px] shadow-brand p-4 overflow-clip border-l-[10px] border-brand-orange">
 
         <View style={{gap: 12}}>
-          <Text className="text-xl font-bold text-brand-navy">Christmas 2023</Text>
+          <Text className="text-xl font-bold text-brand-navy mt-1">Christmas 2023</Text>
           <View className="flex-row justify-start" style={{gap: 12}}>
             <Text className="text-brand-navy">12/24/23</Text>
             <Text className="text-brand-navy opacity-80">12 Songs</Text>
