379fff9ce8cb5d08a29b8ba8bd6cdb37f83d0413
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-30 22:29:33 -0500

    custom TabBar tailwind setup

--- components/TabBar.jsx
+++ components/TabBar.jsx
@@ -1,74 +1,27 @@
 import { View, Text, TouchableOpacity, useWindowDimensions, StyleSheet } from 'react-native';
-import { useEffect, useState } from 'react'
 
 
 export default function TabBar({ state, descriptors, navigation }) {
   const { width } = useWindowDimensions();
-  const [ tabWidth, setTabWidth ] = useState(width - 32);
 
-  function activeBackgroundColor(tab) {
-    if (tab === 0) {
-      return '#91A5CB'
-    } else if (tab === 1) {
-      return '#E9764C'
-    } else if (tab === 2) {
-      return '#FFB56E'
-    }
-  }
-
-  function highlightPosition(tab) {
-    if (tab === 0) {
-      return 'flex-start'
-    } else if (tab === 1) {
-      return 'center'
-    } else if (tab === 2) {
-      return 'flex-end'
-    }
-  }
-
-  const styles = StyleSheet.create({
-    navBar: { 
-      position: 'absolute', 
-      flexDirection: 'row', 
-      justifyContent: 'space-between', 
-      alignItems: 'center', 
-      bottom: 32, 
-      backgroundColor: '#2F3253', 
-      width: tabWidth, 
-      padding: 12, 
-      marginHorizontal: 16, 
-      borderRadius: 14 
-    },
-    buttonWrapper: { 
-      height: 32,
-      width: 84, 
-      borderRadius: 8, 
-      justifyContent: 'center', 
-      alignItems: 'center',
-      zIndex: 1,
-    },
-    navHighlight: {
-      backgroundColor: activeBackgroundColor(state.index),
-      zIndex: 0,
-      height: 32,
-      width: 84, 
-      borderRadius: 8, 
-    },
-    navHighlightWrapper: {
-      bottom: 12,
-      left: 12,
-      position: 'absolute',
-      width: width-58,
-      flexDirection: 'row'
-    }
-  })
+  // access with index positions
+    // color of tab highlight
+  const activeBackgroundColorMap = ['#91A5CB', '#E9764C', '#FFB56E']
+    // position of tab highlight
+  const highlightPositionMap = ['flex-start', 'center', 'flex-end']
 
   return (
     <>
 
-      <View style={styles.navBar}>
-        <View style={[styles.navHighlightWrapper, { justifyContent: highlightPosition(state.index) }]}>
-          <View style={styles.navHighlight}/>
+      <View 
+        className="absolute flex-row justify-between items-center bottom-8 bg-brand-navy p-3 mx-4 rounded-[14px]" 
+        style={{width: width-32}}>
+        <View 
+          className="absolute bottom-3 left-3 flex-row" 
+          style={{ justifyContent: highlightPositionMap[state.index], width: width-58 }}>
+          <View 
+            className="z-0 h-8 w-[84px] rounded-[8px]" 
+            style={{backgroundColor: activeBackgroundColorMap[state.index]}}/>
         </View>
 
         {state.routes.map((route, index) => {
@@ -103,13 +56,13 @@ export default function TabBar({ state, descriptors, navigation }) {
 
           return (
             <TouchableOpacity
+              className="h-8 w-[84px] rounded-[8px] justify-center items-center z-10"
               accessibilityRole="button"
               accessibilityState={isFocused ? { selected: true } : {}}
               accessibilityLabel={options.tabBarAccessibilityLabel}
               testID={options.tabBarTestID}
               onPress={onPress}
               onLongPress={onLongPress}
-              style={[styles.buttonWrapper]}
               key={label}
             >
               <Text style={{ zIndex: 2, fontFamily: 'Poppins_700Bold', color: isFocused ? '#2F3253' : '#fff'}}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontSize: {
       xs: ['12px', '12px'],
