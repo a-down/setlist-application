0406fc36bdb6d33611c53bbeb5ecf7cc4efa42d5
a-down <136649391+a2downing@users.noreply.github.com>
2023-11-01 17:07:57 -0500

    render chords and lyrics from data

--- screens/SongDisplay.jsx
+++ screens/SongDisplay.jsx
@@ -4,6 +4,101 @@ import { View, Text, Pressable, useWindowDimensions } from 'react-native';
 export default function SongDisplay({ navigation }) {
   const { width } = useWindowDimensions();
 
+  const song = {
+    title: 'Come On Get Higher',
+    artist: 'Matt Nathanson',
+    key: 'F',
+    capo: {
+      capoPreferred: true,
+      capoKey: 'C',
+      capoPosition: 7
+    },
+    creator: 'a-down',
+    songData: [
+      {
+        sectionTitle: 'Chorus',
+        sectionLines: [
+          [
+            {
+              content: 'Well you only need the ',
+              type: 'lyrics'
+            },
+            {
+              content: 'F',
+              type: 'chord'
+            },
+            {
+              content: "light when it's burning low",
+              type: 'lyrics'
+            },
+          ],
+          [
+            {
+              content: 'Well you only need the ',
+              type: 'lyrics'
+            },
+            {
+              content: 'F',
+              type: 'chord'
+            },
+            {
+              content: "light when it's burning low",
+              type: 'lyrics'
+            }
+          ],
+          [
+            {
+              content: 'Well you only need the ',
+              type: 'lyrics'
+            },
+            {
+              content: 'F',
+              type: 'chord'
+            },
+            {
+              content: "light when it's burning low",
+              type: 'lyrics'
+            },
+          ],
+        ]
+      },
+    ]
+  }
+
+  console.log(song.songData)
+
+  function SongLineElement({ element }) {
+    console.log('element:', element)
+
+    return element.type === "lyrics" ? (
+      <Text className="text-brand-navy font-medium">{element.content}</Text>
+    ) : (
+      <Text className="text-brand-yellow font-bold">{element.content}</Text>
+    )
+  }
+
+  function SongLine({ line }) {
+    console.log('line:', line)
+    return (
+      <>
+        {line.map((element) => (
+          <SongLineElement element={element} />
+        ))}
+      </>
+    )
+  }
+
+  function SongSection({ section }) {
+    console.log('section:', section)
+    return (
+      <View className="flex-row">
+        {section.sectionLines.map((line) => (
+          <SongLine line={line} />
+        ))}
+      </View>
+    )
+  }
+
   return (
     <View className="flex-1 bg-brand-navy">
       <View className="flex-1 bg-gray-background mt-12 rounded-t-[20px]">
@@ -28,11 +123,11 @@ export default function SongDisplay({ navigation }) {
           </View>
 
           <View className="flex-row w-full mt-4" style={{gap: 16}}>
-            <Pressable className="bg-brand-navy rounded-xl flex-1">
-              <Text className="text-lg font-semibold text-brand-yellow text-center py-4">Capo (7)</Text>
+            <Pressable className="bg-brand-navy rounded-xl flex-1 justify-center">
+              <Text className="text-lg font-semibold text-brand-yellow text-center pt-5 pb-3">Capo (7)</Text>
             </Pressable>
             <Pressable className="bg-transparent rounded-xl flex-1">
-              <Text className="text-lg font-semibold text-brand-yellow text-center py-4">Capo (7)</Text>
+              <Text className="text-lg font-semibold text-brand-navy opacity-60 text-center pt-5 pb-3">No Capo</Text>
             </Pressable>
           </View>
         </View>
@@ -43,6 +138,12 @@ export default function SongDisplay({ navigation }) {
             <Text className="text-brand-navy text-2xl">{'<'}</Text>
             <Text className="text-brand-navy text-lg font-medium mt-1">Chorus</Text>
             <Text className="text-brand-navy text-2xl">{'>'}</Text>
+          </View>
+
+          <View className="bg-gray-300 w-full h-full mt-8">
+            {song.songData.map((section) => (
+              <SongSection section={section} />
+            ))}
           </View>
 
         </View>
